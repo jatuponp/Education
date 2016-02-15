ae86e9dee8b5528fe434b381ee8e953be88359c5
jatuponp <jatuponp@gmail.com>
2016-02-15 15:37:35 +0700

    add new user interface

--- app/src/main/java/com/nkc/education/MainActivity.java
+++ app/src/main/java/com/nkc/education/MainActivity.java
@@ -1,31 +1,165 @@
 package com.nkc.education;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.joanzapata.iconify.Iconify;
+import com.joanzapata.iconify.fonts.FontAwesomeModule;
+import com.nkc.education.adapter.IconAdapter;
+import com.nkc.education.helper.SQLiteHandler;
+import com.nkc.education.helper.SessionManager;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    private SQLiteHandler db;
+    private SessionManager session;
+    Context context;
+    static final String[] iconApps = new String[]{
+            "News", "Exam Schedule", "Documents Service",  "Inbox", "Links", "Feedback", "About us", "Log out"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Iconify.with(new FontAwesomeModule());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        context = getApplicationContext();
+        session = new SessionManager(context);
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        setSupportActionBar(toolbar);
+        Typeface typeFace = Typeface.createFromAsset(getAssets(), "fonts/thaisansneue-regular-webfont.ttf");
+
+        listView = (ListView) findViewById(R.id.list_view);
+
+        listView.setAdapter(new IconAdapter(this, iconApps));
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                switch(position){
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 7:
+                        logoutUser();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+    }
+
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     */
+    private void logoutUser() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        try {
+            //String token = sharedPreferences.getString(QuickstartPreferences.TOKEN_ID, null);
+            // Fetching user details from sqlite
+            HashMap<String, String> user = db.getUserDetails();
+            // Add custom implementation, as needed.
+            Map<String, String> params = new HashMap<String, String>();
+            //params.put("regId", token);
+            params.put("userId", user.get("uid"));
+
+            //String serverUrl = AppConfig.URL_UNREGISTER;
+            //doPost(serverUrl, params);
+            //sharedPreferences.edit().putBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false).apply();
+        } catch (Exception ex) {
+            Log.d("Except", "Failed to complete token refresh" + ex.getMessage());
+        }
+
+        session.setLogin(false);
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private static void doPost(final String urlConn, final Map<String, String> params) {
+        Thread background = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    StringBuilder bodyBuilder = new StringBuilder();
+                    Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
+                    // constructs the POST body using the parameters
+                    while (iterator.hasNext()) {
+                        Map.Entry<String, String> param = iterator.next();
+                        bodyBuilder.append(param.getKey()).append('=')
+                                .append(param.getValue());
+                        if (iterator.hasNext()) {
+                            bodyBuilder.append('&');
+                        }
+                    }
+                    String body = bodyBuilder.toString();
+                    Log.v("POST", "Posting '" + body + "' to " + urlConn);
+                    byte[] bytes = body.getBytes();
+
+                    URL url;
+                    try {
+                        url = new URL(urlConn);
+                    } catch (MalformedURLException e) {
+                        throw new IllegalArgumentException("invalid url: " + urlConn);
+                    }
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestMethod("POST");
+                    con.setDoOutput(true);
+                    con.setFixedLengthStreamingMode(bytes.length);
+                    con.setRequestMethod("POST");
+                    con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+                    con.connect();
+                    // post the request
+                    OutputStream out = con.getOutputStream();
+                    out.write(bytes);
+                    out.flush();
+                    out.close();
+
+                    int status = con.getResponseCode();
+                    Log.e("status", String.valueOf(status));
+
+                } catch (Throwable t) {
+                    // just end the background thread
+                    Log.i("Animation", "Thread  exception " + t);
+                }
+            }
+
+        });
+        // Start Thread
+        background.start();
     }
 
     @Override
---
+++ app/src/main/java/com/nkc/education/adapter/IconAdapter.java
@@ -0,0 +1,102 @@
+package com.nkc.education.adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.nkc.education.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by Jumpon-pc on 15/2/2559.
+ */
+public class IconAdapter extends BaseAdapter {
+    private Context context;
+    private final String[] iconValues;
+
+    public IconAdapter(Context context, String[] iconValues){
+        this.context = context;
+        this.iconValues = iconValues;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        Typeface typeFace = Typeface.createFromAsset(context.getAssets(), "fonts/thaisansneue-regular-webfont.ttf");
+        View gridView;
+
+        if (convertView == null) {
+
+            gridView = new View(context);
+
+            // get layout from mobile.xml
+            gridView = inflater.inflate(R.layout.grid_layout, null);
+
+            TextView txtTitleEN = (TextView) gridView.findViewById(R.id.titleEN);
+            TextView textView = (TextView) gridView.findViewById(R.id.grid_item_label);
+            TextView txtTitleTH = (TextView) gridView.findViewById(R.id.titleTH);
+            txtTitleEN.setText(iconValues[position]);
+            txtTitleEN.setTypeface(typeFace);
+            txtTitleTH.setTypeface(typeFace);
+
+            //"{fa-calendar #3BAFDA 50sp}", "{fa-bullhorn #3BAFDA 50sp}", "{fa-envelope #3BAFDA 50sp}", "{fa-comments #3BAFDA 50sp}", "{fa-phone #3BAFDA 50sp}", "{fa-globe #3BAFDA 50sp}", "{fa-info #3BAFDA 50sp}", "{fa-power-off #3BAFDA 50sp}"};
+            String icon = iconValues[position];
+
+            if (icon.equals("Exam Schedule")) {
+                textView.setText("{fa-calendar #3BAFDA 50sp}");
+                txtTitleTH.setText("ตรวจสอบตารางสอบ");
+            } else if (icon.equals("News")) {
+                textView.setText("{fa-bullhorn #3BAFDA 50sp}");
+                txtTitleTH.setText("ข่าวประชาสัมพันธ์");
+            } else if (icon.equals("Inbox")) {
+                textView.setText("{fa-envelope #3BAFDA 50sp}");
+                txtTitleTH.setText("กล่องข้อความเข้า");
+            } else if (icon.equals("Feedback")) {
+                textView.setText("{fa-bug #3BAFDA 50sp}");
+                txtTitleTH.setText("รายงานข้อผิดพลาด");
+            } else if (icon.equals("Documents Service")) {
+                textView.setText("{fa-book #3BAFDA 50sp}");
+                txtTitleTH.setText("หมายเลขโทรศัพท์");
+            } else if (icon.equals("Links")) {
+                textView.setText("{fa-globe #3BAFDA 50sp}");
+                txtTitleTH.setText("เว็บไซต์ที่เกี่ยวข้อง");
+            } else if (icon.equals("About us")) {
+                textView.setText("{fa-info #3BAFDA 50sp}");
+                txtTitleTH.setText("เกี่ยวกับโปรแกรม");
+            } else if (icon.equals("Log out")) {
+                textView.setText("{fa-power-off #3BAFDA 50sp}");
+                txtTitleTH.setText("ออกจากระบบ");
+            } else {
+                textView.setText("");
+            }
+
+        } else {
+            gridView = (View) convertView;
+        }
+
+        return gridView;
+    }
+
+    @Override
+    public int getCount() {
+        return iconValues.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+}
